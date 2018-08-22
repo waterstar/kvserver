aa768b12a5f6652d7903a630ae4b8a4f57bd862c
suising.lai <suising.lai@ihealthlabs.com>
2018-08-22 21:34:32 +0800

    more test cases

--- __tests__/api_spec.js
+++ __tests__/api_spec.js
@@ -3,6 +3,7 @@ import frisby from 'frisby';
 const host = 'localhost:3000';
 let timestamp = '0';
 
+/** happy path ***/
 it ('POST 1 key with 2 different values in sequence, expect 201 Created', function () {
    return frisby
       .post(`http://${host}/object`, {
@@ -10,7 +11,9 @@ it ('POST 1 key with 2 different values in sequence, expect 201 Created', functi
       })
       .expect('status', 201)
       .then(()=>{
+         // time between two key-value records
          timestamp = Date.now();
+
          return frisby
             .post(`http://${host}/object`, {
                'hello': 'konichiwa2',
@@ -30,4 +33,16 @@ it('Get back the key-value added', function () {
   return frisby.get(`http://${host}/object/hello?timestamp=${timestamp}`)
       .expect('status', 200)
       .expect('json', 'value', 'konichiwa');
+});
+
+/** error cases **/
+
+it('Get unsupported root url', function () {
+  return frisby.get(`http://${host}`)
+      .expect('status', 404);
+});
+
+it('Get unsupported url', function () {
+  return frisby.get(`http://${host}/object`)
+      .expect('status', 400);
 });
